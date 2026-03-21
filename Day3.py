33dc14a00f5f981107c6dd5abaaf8d0ac957d5dc
Renukasarvade <renukasarvade2903@gmail.com>
2026-03-21 20:40:13 +0530

    Day 3: Conditional logic projects (rollercoaster, pizza delivery, treasure island game)

---
+++ Day3.py
@@ -0,0 +1,139 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    age = int(input("What is your age? "))
+    if age < 12:
+        bill = 5
+        print("Child tickets are $5.")
+    elif age <= 18:
+        bill = 7
+        print("Youth tickets are $7.")
+    else:
+        bill = 12
+        print("Adult tickets are $12.")
+
+    wants_photo = input("Do you want a photo taken? Y or N. ")
+    if wants_photo == "Y":
+        bill += 3
+
+    print(f"Your final bill is ${bill}")
+
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+
+
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M or L: ")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+
+bill=0
+if size == "S":
+    bill+=10
+elif size == "M":
+    bill+=20
+elif size == "L":
+    bill+=25
+else:
+    print("You enter worng input")
+
+if pepperoni == "Y":
+    if size == "S":
+        bill+=2
+    elif size == "M":
+        bill+=1
+else :
+    print("You enter worng input")
+if extra_cheese == "Y":
+    bill+=1
+print("Your final bill is:",bill)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(r'''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\ ` . "-._ /_______________|_______
+|                   | |o ;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+print(r'''
+         |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\ ` . "-._ /_______________|_______
+|                   | |o ;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+*******************************************************************************
+''')
+
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+# Step 1
+choice1 = input("You're at a crossroad. Where do you want to go? Type 'left' or 'right': ").lower()
+
+if choice1 == "left":
+
+    # Step 2
+    choice2 = input("You've come to a lake. Type 'wait' to wait for a boat or 'swim' to swim across: ").lower()
+
+    if choice2 == "wait":
+
+        # Step 3
+        choice3 = input("You arrive at 3 doors. One red, one blue, one yellow. Which color? ").lower()
+
+        if choice3 == "yellow":
+            print("🎉 You Win! You found the treasure!")
+        elif choice3 == "red":
+            print("🔥 Burned by fire. Game Over.")
+        elif choice3 == "blue":
+            print("🐊 Eaten by beasts. Game Over.")
+        else:
+            print("🚪 Wrong choice. Game Over.")
+
+    else:
+        print("🌊 Attacked by trout. Game Over.")
+
+else:
+    print("🕳️ Fell into a hole. Game Over.")
